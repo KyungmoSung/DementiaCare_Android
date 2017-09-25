7dc21d500fd555356e76efb913f3cda872d70a35
Minwoo <pmw9027@naver.com>
2017-09-25 13:07:43 +0900

    update notification func

--- app/src/main/java/com/example/sung/dementiacare/SubMenuNotificationActivity.java
+++ app/src/main/java/com/example/sung/dementiacare/SubMenuNotificationActivity.java
@@ -1,17 +1,69 @@
 package com.example.sung.dementiacare;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.sung.dementiacare.notification.NotificationMedicineActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Sung on 2017. 9. 3..
  */
 
 public class SubMenuNotificationActivity extends AppCompatActivity {
+
+    Intent intent;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_submenu_notification);
+        setContentView(R.layout.activity_submenu_support);
+
+        ButterKnife.bind(this);
+
+
+        TextView tv1 = (TextView)findViewById(R.id.textView2);
+        TextView tv2 = (TextView)findViewById(R.id.textView3);
+        TextView tv3 = (TextView)findViewById(R.id.textView4);
+        TextView tv4 = (TextView)findViewById(R.id.textView5);
+
+        tv1.setText("약물");
+        tv2.setText("일정");
+        tv3.setText("오늘의 정보");
+        tv4.setText("오늘의 희망 메세지");
+
     }
+
+    @OnClick(R.id.textView2)
+    void onClickLayer1(){
+
+        intent = new Intent(getApplicationContext(), NotificationMedicineActivity.class);
+        startActivity(intent);
+
+    }
+
+    @OnClick(R.id.textView3)
+    void onClickLayer2(){
+
+
+    }
+
+    @OnClick(R.id.textView4)
+    void onClickLayer3(){
+
+    }
+
+    @OnClick(R.id.textView5)
+    void onClickLayer4(){
+
+
+    }
+
 }
